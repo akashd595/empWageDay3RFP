713e44304de5db8fca09bcec576ebce58e46b261
Akash Das <akashd595@gmail.com>
2022-05-20 21:46:40 +0530

    ADDED: Attendence of the Employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,5 +4,17 @@ import java.util.*;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        int IS_FULL_DAY = 1;
+        int IS_HALF_DAY = 2;
+        int IS_ABSENT_TODAY = 0;
+
+        if (attendance == IS_ABSENT_TODAY) {
+            System.out.println("Employee is absent today");
+        } else {
+            System.out.println("Employee is Present today");
+        }
+
     }
 }
