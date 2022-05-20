eb2b36648db88cfd968cf3fc5e7757390cb7c7f8
Akash Das <akashd595@gmail.com>
2022-05-20 22:28:04 +0530

    ADDED: Added switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,11 +6,10 @@ public class EmployeeWage {
 
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         Random random = new Random();
-
         int attendance = random.nextInt(3);
-        int IS_FULL_DAY = 1;
-        int IS_HALF_DAY = 2;
-        int IS_ABSENT_TODAY = 0;
+        final int IS_FULL_DAY = 1;
+        final int IS_HALF_DAY = 2;
+        final int IS_ABSENT_TODAY = 0;
         int FULL_DAY_HOUR = 8;
         int HALF_DAY_HOUR = 4;
         int ABSENT_DAY_HOUR = 0;
@@ -18,18 +17,20 @@ public class EmployeeWage {
         int total_empl_hours = 0;
         int total_empl_wage = 0;
 
-        if (attendance == IS_ABSENT_TODAY) {
-            System.out.println("Employee is absent today");
-        } else if (attendance == IS_HALF_DAY) {
-            System.out.println("Employee is Half today");
-            total_empl_hours += HALF_DAY_HOUR;
-            total_empl_wage += WAGE_PER_HOUR*HALF_DAY_HOUR;
-        } else {
-            System.out.println("Employee is Present today");
-            total_empl_hours += FULL_DAY_HOUR;
-            total_empl_wage += WAGE_PER_HOUR*FULL_DAY_HOUR;
+        switch (attendance) {
+            case IS_FULL_DAY :
+                total_empl_hours = FULL_DAY_HOUR;
+                break;
+            case IS_HALF_DAY :
+                total_empl_hours = HALF_DAY_HOUR;
+                break;
+            case IS_ABSENT_TODAY :
+                total_empl_hours = ABSENT_DAY_HOUR;
+                break;
+            default:
+                total_empl_hours = ABSENT_DAY_HOUR;
         }
-
+        total_empl_wage += WAGE_PER_HOUR*total_empl_hours;
         System.out.println("Total wage:- "+total_empl_wage);
 
     }
